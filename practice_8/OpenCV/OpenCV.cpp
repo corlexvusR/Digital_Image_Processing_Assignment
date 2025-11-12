3f4ade77893e445d5406f70ae42135ae9bb11431
corlexvusR <flteswou@gmail.com>
2025-11-12 19:13:37 +0900

    feat: 실습 8 프로젝트 추가 - 구성 속성에 opencv 관련 설정

---
+++ practice_8/OpenCV/OpenCV.cpp
@@ -0,0 +1,16 @@
+﻿#include <opencv2\opencv.hpp>
+#include <opencv2\highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+    Mat image = imread("test.png", IMREAD_COLOR);
+
+    imshow("Window", image);
+
+    waitKey();
+
+    return 0;
+}
